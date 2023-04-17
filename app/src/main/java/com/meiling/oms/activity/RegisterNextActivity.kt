0d836263e72c2b084e01aa631af1a60f3ff30738
qiaoyangyang <qiaoyang_1004@163.com>
2023-04-17 15:35:38 +0800

    feat 注册第二步

--- app/src/main/java/com/meiling/oms/activity/RegisterNextActivity.kt
+++ app/src/main/java/com/meiling/oms/activity/RegisterNextActivity.kt
@@ -8,10 +8,14 @@ import android.view.View
 import androidx.core.widget.addTextChangedListener
 import com.luck.picture.lib.PictureSelector
 import com.luck.picture.lib.config.PictureMimeType
+import com.luck.picture.lib.entity.LocalMedia
+import com.luck.picture.lib.listener.OnResultCallbackListener
 import com.meiling.common.activity.BaseActivity
 import com.meiling.common.activity.BaseVmActivity
 import com.meiling.common.activity.BaseVmDbActivity
 import com.meiling.common.network.service.loginService
+import com.meiling.common.utils.GlideAppUtils
+import com.meiling.common.utils.GlideEngine
 import com.meiling.common.utils.InputTextManager
 import com.meiling.oms.databinding.ActivityRegisterBinding
 import com.meiling.oms.databinding.ActivityRegisterNextBinding
@@ -30,9 +34,28 @@ class RegisterNextActivity : BaseActivity<RegisterViewModel, ActivityRegisterNex
 
 
         mDatabind.addImg.setOnClickListener {
-//            PictureSelector.create(this)
-//                .openGallery(PictureMimeType.ofImage())
-//                .imageEngine(GlideEn)
+            PictureSelector.create(this)
+                .openGallery(PictureMimeType.ofImage())
+                .imageEngine(GlideEngine.createGlideEngine())
+                .maxSelectNum(1)
+                .minSelectNum(1)
+                .isReturnEmpty(true)
+                .isDisplayOriginalSize(true)
+                .isPreviewImage(true)
+                .minimumCompressSize(2000)
+                .cutOutQuality(90)
+                .forResult(object :OnResultCallbackListener<LocalMedia>{
+                    override fun onResult(result: MutableList<LocalMedia>?) {
+                        if(result?.isNotEmpty() == true){
+                            GlideAppUtils.loadUrl(mDatabind.addImg,result.get(0).path)
+                        }
+                    }
+
+                    override fun onCancel() {
+
+                    }
+
+                })
         }
 
 
