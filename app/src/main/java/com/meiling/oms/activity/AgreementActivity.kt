4765dc986ead0d2194f8228c4c6ef1b9addbdedf
lwq <13993365940@163.com>
2023-04-21 17:36:52 +0800

    feat UI修改

--- app/src/main/java/com/meiling/oms/activity/AgreementActivity.kt
+++ app/src/main/java/com/meiling/oms/activity/AgreementActivity.kt
@@ -3,9 +3,7 @@ package com.meiling.oms.activity
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.webkit.WebResourceRequest
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.webkit.*
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.meiling.common.activity.BaseActivity
 import com.meiling.oms.databinding.ActivityAgreementBinding
@@ -31,18 +29,34 @@ class AgreementActivity : BaseActivity<VoucherinspectionViewModel, ActivityAgree
         }
 
         // 载入内容
-        mDatabind.mWebView.loadUrl(url);
+//        mDatabind.mWebView.loadUrl(url);
+//        val webView: WebView = findViewById(R.id.webView)
 
-//        mDatabind.mWebView.webViewClient = object :WebViewClient(){
-//            override fun shouldOverrideUrlLoading(
-//                view: WebView?,
-//                request: WebResourceRequest?
-//            ): Boolean {
-//                return super.shouldOverrideUrlLoading(view, request)
-//                    view?.loadUrl(url);
-//            }
-//
-//        }
+// 启用JavaScript执行
+        mDatabind.mWebView.settings.javaScriptEnabled = true
+
+// 允许网页缩放
+        mDatabind.mWebView.settings.setSupportZoom(true)
+        mDatabind.mWebView.settings.builtInZoomControls = true
+        mDatabind.mWebView.settings.displayZoomControls = false
+
+// 缓存设置
+//         mDatabind.mWebView.settings.cacheMode = WebSettings.LOAD_DEFAULT
+//         mDatabind.mWebView.settings.setAppCacheEnabled(true)
+//         mDatabind.mWebView.settings.setAppCachePath(cacheDir.path)
+
+// 允许混合内容（HTTP和HTTPS）
+        mDatabind.mWebView.settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+
+// 允许在 mDatabind.mWebView中打开链接，而不是打开默认的浏览器
+//         mDatabind.mWebView.webViewClient = WebViewClient()
+
+// 允许在WebView中打开多个窗口
+        mDatabind.mWebView.webChromeClient = WebChromeClient()
+
+// 加载网页链接
+        mDatabind.mWebView.loadUrl("http://dev-oms.igoodsale.com/#/userAgreement")
+//        mDatabind.mWebView.loadUrl("https://blog.csdn.net/qq_43515862/article/details/110293455")
 
 
     }
