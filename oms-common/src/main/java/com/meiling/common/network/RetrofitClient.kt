c0ffa53dd3f3e44534a2360d5ff94d44a89fb4f2
yjk <15535958281@163.com>
2023-04-24 14:58:42 +0800

    feat 渠道店铺

--- oms-common/src/main/java/com/meiling/common/network/RetrofitClient.kt
+++ oms-common/src/main/java/com/meiling/common/network/RetrofitClient.kt
@@ -37,9 +37,9 @@ class RetrofitClient {
 
     private fun provideRetrofit(client: OkHttpClient): Retrofit {
         return Retrofit.Builder()
-//            .baseUrl("http://test-oms-api.igoodsale.com")//测试环境
+            .baseUrl("http://test-oms-api.igoodsale.com")//测试环境
 //            .baseUrl("http://dev-oms-api.igoodsale.com")//开发环境
-            .baseUrl("https://ods-api.igoodsale.com")//生产环境
+           // .baseUrl("https://ods-api.igoodsale.com")//生产环境
 //            .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
             .addConverterFactory(CustomGsonConverterFactory.create())
