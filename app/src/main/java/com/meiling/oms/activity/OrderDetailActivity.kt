1007d125d220f621feb0950448bbe3dd5ee01b3a
yjk <15535958281@163.com>
2023-05-30 11:37:07 +0800

    feat bug修改

--- app/src/main/java/com/meiling/oms/activity/OrderDetailActivity.kt
+++ app/src/main/java/com/meiling/oms/activity/OrderDetailActivity.kt
@@ -1,8 +1,13 @@
 package com.meiling.oms.activity
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
+import android.view.WindowManager
 import com.amap.api.maps.AMap
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetBehavior.BottomSheetCallback
 import com.meiling.common.activity.BaseActivity
 import com.meiling.oms.databinding.ActivityOrderDetailBinding
 import com.meiling.oms.viewmodel.OrderCreateViewModel
@@ -17,6 +22,25 @@ class OrderDetailActivity : BaseActivity<OrderCreateViewModel, ActivityOrderDeta
         if (aMap == null) {
             aMap = mDatabind.map.map
         }
+       BottomSheetBehavior.from(mDatabind.bottomSheet).setBottomSheetCallback(object :BottomSheetCallback(){
+           override fun onStateChanged(bottomSheet: View, newState: Int) {
+               val linearParams = mDatabind.map.layoutParams //取控件textView当前的布局参数
+
+               if (newState == BottomSheetBehavior.STATE_EXPANDED || newState == BottomSheetBehavior.STATE_HALF_EXPANDED ) {
+                   linearParams.height=800
+                   Log.d("yjk","0000")
+               }else if (newState == BottomSheetBehavior.STATE_COLLAPSED) {
+                   linearParams.height= WindowManager.LayoutParams.MATCH_PARENT
+                   Log.d("yjk","2222")
+               }
+               mDatabind.map.layoutParams=linearParams
+
+           }
+
+           override fun onSlide(bottomSheet: View, slideOffset: Float) {
+           }
+
+       })
     }
 
     override fun getBind(layoutInflater: LayoutInflater): ActivityOrderDetailBinding {
