6a056c92a503426b132f77b14fd0bcc593b9d15a
qiaoyangyang <qiaoyang_1004@163.com>
2023-06-05 18:39:14 +0800

    feat 物流充值

--- app/src/main/java/com/meiling/oms/activity/LogisticsRechargeActivity.kt
+++ app/src/main/java/com/meiling/oms/activity/LogisticsRechargeActivity.kt
@@ -4,17 +4,13 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.widget.ImageView
-import com.blankj.utilcode.util.ActivityUtils
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
 import com.meiling.common.activity.BaseActivity
 import com.meiling.common.network.data.*
 import com.meiling.common.network.service.loginService
-import com.meiling.common.network.service.meService
 import com.meiling.common.utils.GlideAppUtils
-import com.meiling.common.utils.TextDrawableUtils
 import com.meiling.oms.R
 import com.meiling.oms.databinding.ActivityLogistcsRechargeLayoutBinding
 import com.meiling.oms.dialog.*
@@ -28,7 +24,6 @@ class LogisticsRechargeActivity :
     BaseActivity<LogisticsRechargeViewModel, ActivityLogistcsRechargeLayoutBinding>() {
 
     lateinit var adapter: BaseQuickAdapter<BalanceItem, BaseViewHolder>
-    var tenantId = ""
     var poid = ""
     var type=""
     override fun initView(savedInstanceState: Bundle?) {
@@ -45,7 +40,6 @@ class LogisticsRechargeActivity :
     @SuppressLint("SuspiciousIndentation")
     override fun initData() {
         super.initData()
-        tenantId = intent?.getStringExtra("tenantId") ?: ""
         poid = intent?.getStringExtra("poid") ?: ""
 
         getMerchantBalance()
@@ -87,8 +81,6 @@ class LogisticsRechargeActivity :
                         holder?.setText(R.id.txtShopName,item.stationCommonId.get(0).name)
                     }
                 }
-
-
             }
         }
 
@@ -97,17 +89,14 @@ class LogisticsRechargeActivity :
             click(adapte,position)
         }
 
-
-
         mDatabind.recyClerView.adapter = adapter
-
 
     }
 
     private fun click(adapte: BaseQuickAdapter<*, *>, position: Int) {
         var merchant = adapte.data.get(position) as BalanceItem
 
-        if (merchant.channelType == "uu") {
+//        if (merchant.channelType == "uu") {
             mViewModel?.launchRequest(
                 {
                     loginService.merchantRecharge(MerchantRecharge("1","H5",merchant.channelType,merchant.stationCommonId.get(0).id))
@@ -119,76 +108,7 @@ class LogisticsRechargeActivity :
                     it?.let { showToast(it) }
                 }
             )
-
-        } else {
-            when (merchant.channelType) {
-                "dada" -> {
-                    var bindingOther = BindDadaOtherLogistics()
-                        .newInstance(
-                            "已有达达快送APP账号",
-                            "达达快送APP帐号授权后即可发单，与达达里价格、优惠等活动一致。","dada")
-                    bindingOther.setMySureOnclickListener {type,type2->
-                        if(type2=="1"){
-                            getUrl(type)
-
-                        }else{
-                            //TODO 去注册
-                            var dialogRegistDadaLogistics=DialogRegistDadaLogistics()
-                            dialogRegistDadaLogistics.show(supportFragmentManager)
-                        }
-                    }
-                    bindingOther.show(supportFragmentManager)
-                }
-                "sf_tc" -> {
-                    var bindingOther = BindOtherLogistics().newInstance("已有顺丰同城账号",
-                        "顺丰同城帐号授权后即可发单，与顺丰同城里价格、优惠等活动一致。\n如果没有账号，请先下载顺丰同城APP，注册并开通商户版。")
-                    bindingOther.setMySureOnclickListener {
-                        getUrl("sf_tc")
-                    }
-                    bindingOther.show(supportFragmentManager)
-                }
-                "ss" -> {
-                    var bindingOther = BindOtherLogistics().newInstance("已有闪送账号",
-                        "闪送帐号授权后即可发单，与闪送里价格、优惠等活动一致。\n如果没有账号，请先下载闪送商家版APP后，注册账号。")
-                    bindingOther.setMySureOnclickListener {
-                        getUrl("ss")
-                    }
-                    bindingOther.show(supportFragmentManager)
-                }
-
-            }
-        }
-    }
-
-
-
-    /**
-     * 手动获取三方门店列表
-     */
-    fun getShopList(type: String) {
-//        var otherShopListDialog = OtherShopListDialog().newInstance(poid)
-//        otherShopListDialog.setMySureOnclickListener {
-//            showLoading("正在绑定")
-//            mViewModel.launchRequest(
-//                { loginService.bindShop(poid, it.thirdShopId, it.thirdShopName, type) },
-//                onSuccess = {
-//                    showToast("绑定成功")
-//                    getLogisticsList(poid)
-//                    disLoading()
-//                },
-//                onError = {
-//                    disLoading()
-//                    it?.let { showToast(it) }
-//                }
-//            )
 //        }
-//        otherShopListDialog.show(supportFragmentManager)
-
-        var chooseViewDialog=ChooseViewDialog().newInstance("配送平台")
-        chooseViewDialog.setMySureOnclickListener {
-            showToast(it.typeName)
-        }
-        chooseViewDialog.show(supportFragmentManager)
     }
 
 
@@ -204,28 +124,11 @@ class LogisticsRechargeActivity :
                 it?.let { adapter?.setList(it) }
             },
             onError = {
-
+                adapter?.data?.clear()
             }
         )
     }
 
-    /**
-     * 获取授权链接
-     */
-    fun getUrl(type: String) {
-        mViewModel.launchRequest(
-            {
-                loginService.getUrl(originId = null, poid, type)
-            },
-            true,
-            onSuccess = {
-                startActivity(Intent(this, BaseWebActivity::class.java).putExtra("url", it))
-            },
-            onError = {
-                it?.let { it1 -> showToast(it1) }
-            }
-        )
-    }
 
     override fun createObserver() {
 
